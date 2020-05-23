024cdda1944c3324eded3b944feb9a44ec5097dc
matthew <matthew.sloan23gmail.com>
2020-05-23 00:42:00 -0500

    Integrating bootstrap into react

---
+++ src/Components/component1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Button} from 'react-bootstrap';
+
+
+ export function buttonFromJSXFile (){
+     return(
+        <Button>
+        This is a button from a seperate jsx file
+       </Button>
+     )
+ }
+ 
